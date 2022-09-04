8f1628bbd6d1f42117a260f4b26a0011c9890e7b
Deepak <raazd997@gmail.com>
2022-09-04 17:35:50 +1000

    image link updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/post'
 import Link from 'next/link'
 import Date from '../components/date'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const allPostsData = getSortedPostsData()
   return {
     props: {
       allPostsData,
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/post'
 import Date from '../../components/date';
